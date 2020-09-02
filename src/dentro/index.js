d0db81478b4f551d9dd325a887d5f39ad07fb5c8
Murilo Rech <muriloqr@hotmail.com>
2020-09-01 21:50:37 -0300

    Logica salva lista

--- src/dentro/index.js
+++ src/dentro/index.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Ionicons } from '@expo/vector-icons'
 import { Text, StyleSheet, SafeAreaView, Keyboard, TextInput, Animated, TouchableOpacity, View, AsyncStorage, Alert} from 'react-native'
 
@@ -7,26 +7,44 @@ export default function Dentro() {
     const [peixe, setPeixe] = useState('')
     const [tamanho, setTamanho] = useState('')
     const [peso, setPeso] = useState('')
+    const [listaDados, setListaDados] = useState([])
+
+    useEffect(() => {
+        (async () => {
+            let listaDadosStorage = await AsyncStorage.getItem("listaDados")
+            
+            if (listaDadosStorage) { // Se for verdadeiro, é pq existe um array
+                 listaDadosStorage = JSON.parse(listaDadosStorage) // Parseando o array
+                 setListaDados(listaDadosStorage)
+            }
+        })()
+
+    }); // Isso faz toda vez que entrar no componente exibir a lista, senao seria necessario salvar pra aparecer
 
     async function salvar() {
        const dados = {
         peixe, tamanho, peso
        }
        
-        await AsyncStorage.setItem("dados", JSON.stringify(dados))
-    }
+       let listaDadosStorage = await AsyncStorage.getItem("listaDados")
 
-    async function mostrarDados() {
-        const json = await AsyncStorage.getItem("dados")
-        const dados = JSON.parse(json)
-    
-    Alert.alert('Especificações do peixe',
-        `Nome: ${peixe} - Tamanho ${tamanho} - Peso ${peso}`
-    
-    )}
-   
-   
-   
+       if (listaDadosStorage) { // Se for verdadeiro, é pq existe um array
+
+            listaDadosStorage = JSON.parse(listaDadosStorage) // Parseando o array
+
+            listaDadosStorage.push(dados) // Adiciona no array
+       } else { // Se não é verdadeiro, logo nao existe o array ainda, vamos cria-lo
+            listaDadosStorage = [dados]
+
+        //    Exemplo descritivo
+        // listaDados = [
+        //    { peixe: "Tal", tamanho: 513, peso: 241 }
+        // ]
+       }
+        
+       await AsyncStorage.setItem("listaDados", JSON.stringify(listaDadosStorage))
+       setListaDados(listaDadosStorage)
+    }
    
     return (
         
@@ -37,7 +55,6 @@ export default function Dentro() {
             <View style={styles.content}> 
                 <Text style={styles.title}> Peixes Capturados </Text> 
             </View>
-
 
             <View style={{ width: '100%', justifyContent: 'center', alignItems: 'center'}} >
                 <TextInput 
@@ -65,19 +82,31 @@ export default function Dentro() {
                     onChangeText={text => setPeso(text)}
                 />
 
-                <TouchableOpacity style={styles.btnSend} onPress={salvar} >
+                <TouchableOpacity style={styles.btnSend} onPress={() => salvar()} >
                     <Text style={styles.send} >
                         Enviar
                     </Text>           
                 </TouchableOpacity>
 
-                <TouchableOpacity style={styles.btnSend} onPress={mostrarDados} >
+                <TouchableOpacity style={styles.btnSend} onPress={() => mostrarDados()} >
                     <Text style={styles.send} >
                         Mostrar informações
                     </Text>           
                 </TouchableOpacity>
 
-
+                {listaDados.map((dado, index) => 
+                    <View key={index}>
+                        <Text>
+                            Nome peixe: {dado.peixe}
+                        </Text>
+                        <Text>
+                            Tamanho peixe: {dado.tamanho}
+                        </Text>
+                        <Text>
+                            Peso peixe: {dado.peso}
+                        </Text>
+                    </View>
+                )} 
 
             </View>
 
